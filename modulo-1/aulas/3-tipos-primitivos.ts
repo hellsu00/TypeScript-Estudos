5486c2626fa8ccf490eb99f6852ce456e92ca407
Felipe Costa <f.costa00@outlook.com>
2023-10-27 00:13:40 -0300

    tipos-primitivos

---
+++ modulo-1/aulas/3-tipos-primitivos.ts
@@ -0,0 +1,13 @@
+{
+    const nome: string = 'Felipe'
+    const idade: number = 29
+    const programmer: boolean = true
+
+    function ola(nome: string): void {
+        console.log(`olá ${nome}`)
+    }
+
+    class Pessoa {
+        nome: string = 'Felipe'
+    }
+}
---
+++ modulo-1/aulas/4-inferencia.ts
@@ -0,0 +1,13 @@
+{
+    const nome = 'Felipe'
+    const idade = 29
+    const programmer = true
+
+    function ola(nome: string): void {
+        console.log(`olá ${nome}`)
+    }
+    let arr = [1, 'Felipe', true, null]
+
+    const numeros = [1,2,3,4,5,6]
+    const soma = numeros.reduce((acc, num) => acc + num)
+}
