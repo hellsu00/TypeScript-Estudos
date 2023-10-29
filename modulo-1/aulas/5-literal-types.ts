b840ef78bd6b501d18a92ad37d2e8d1b69362999
Felipe Costa <f.costa00@outlook.com>
2023-10-28 23:43:27 -0300

    literal types

---
+++ modulo-1/aulas/5-literal-types.ts
@@ -0,0 +1,12 @@
+{
+    const nome = 'Ana'
+    const idade = 29
+    const programmer = true
+
+    function ola(nome: 'Felipe'): void {
+        console.log(`olá ${nome}`)
+    }
+
+    const sempreTrue: true = true
+    const sempreFalse: false = false
+}
