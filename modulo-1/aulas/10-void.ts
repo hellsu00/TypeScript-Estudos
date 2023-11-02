109d3250335751a76615b4627adc2ff96e4c5ff4
Felipe Costa <f.costa00@outlook.com>
2023-11-01 22:17:28 -0300

    feat: add tipo nulos

---
+++ modulo-1/aulas/10-void.ts
@@ -0,0 +1,5 @@
+{
+    const u: undefined = undefined
+    const outro: null = u
+    const v: void = u
+}
---
+++ modulo-1/aulas/8-unknown.ts
@@ -0,0 +1,17 @@
+{
+    let a: string = 'Felipe'
+    const b = 23
+    let c: unknown = a
+    c = b
+    c = null
+    c = undefined
+    c = {}
+    c = []
+    c = c
+    c = void 0
+    c = 100n
+
+    const x = c
+
+    const o: string = c
+}
