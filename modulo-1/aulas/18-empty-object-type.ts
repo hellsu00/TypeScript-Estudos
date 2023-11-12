33780d360d8e3f63a746bfed03a7d991f9e82364
Felipe Costa <f.costa00@outlook.com>
2023-11-11 22:04:48 -0300

    feat: empty object type

---
+++ modulo-1/aulas/18-empty-object-type.ts
@@ -0,0 +1,13 @@
+{
+    let obj: {} = {}
+    obj = 'a'
+    obj = 1
+    obj = true
+    obj = []
+    obj = () => { }
+    obj = null
+    obj = undefined
+
+    const a: {} = {}
+    a.toString()
+}
